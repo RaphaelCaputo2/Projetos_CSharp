cbd2121b4344046b8cad00f813d9e6a0b3bf32b4
Raphael Caputo <faelelf2@gmail.com>
2021-03-30 00:18:35 -0300

    Configuracao do projeto

--- Projeto_CSharp/newclass.cs
+++ Projeto_CSharp/newclass.cs
@@ -2,6 +2,9 @@ namespace raphael.Documentos.Projetos_C#.Projeto_CSharp
 {
     public class newclass
     {
-        
+           static void Main(string[] args)
+      {
+        Console.WriteLine("Hello World")
+      }
     }
 }
