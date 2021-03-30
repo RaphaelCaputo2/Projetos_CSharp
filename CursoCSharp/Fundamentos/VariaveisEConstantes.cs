6194892680f5382823de77b759c59c464dff1950
Raphael Caputo <faelelf2@gmail.com>
2021-03-30 19:51:40 -0300

    Variaveis e constantes 2

--- CursoCSharp/Fundamentos/VariaveisEConstantes.cs
+++ CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -2,18 +2,39 @@ using System;
 
 namespace CursoCSharp.Fundamentos
 {
-    public class VariaveisEConstantes
+  public class VariaveisEConstantes
+  {
+    public static void Executar()
     {
-        public static void Executar(){
-            double raio = 4.5;
-            const double PI = 3.14;
+      double raio = 4.5;
+      const double PI = 3.14;
 
 
-    raio = 5.5;
-            double area = PI * raio * raio;
-            System.Console.WriteLine(area);
-            System.Console.WriteLine("Área é " + area);
+      raio = 5.5;
+      double area = PI * raio * raio;
+      System.Console.WriteLine(area);
+      System.Console.WriteLine("Área é " + area);
 
-        }
+      // Tipos internos
+
+      bool estaChovendo = true;
+      System.Console.WriteLine("Está chovendo " + estaChovendo);
+
+      byte idade = 32;
+      System.Console.WriteLine("Minha idade é " + idade);
+
+      sbyte saldoDeGols = sbyte.MinValue;
+      System.Console.WriteLine("Saldo de Gols " + saldoDeGols);
+
+
+      short salario = short.MinValue;
+      System.Console.WriteLine("Salário " + salario);
+
+      int menorValorInt = int.MinValue; // Mais usado dos inteiros!
+      System.Console.WriteLine("Menor int " + menorValorInt);
+
+
+
     }
+  }
 }
