7913b33d9bd1d4abb3c97e437616985ce49cadce
Raphael Caputo <faelelf2@gmail.com>
2021-03-30 03:09:13 -0300

    Finalizando

---
+++ teste/firstProgram/newclass.cs
@@ -0,0 +1,12 @@
+namespace teste.firstProgram
+{
+  public class newclass
+  {
+    static void Main()
+    {
+      System.Console.Write("Hello World!");
+      System.Console.WriteLine("Segundo Hello World UHUUU");
+      System.Console.WriteLine("POSSO DORMIR EU CONSEGUIIIII");
+    }
+  }
+}
