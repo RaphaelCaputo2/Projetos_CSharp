8c2dc7a195f1246f8ee3ea27cd8677ac96f688be
Raphael Caputo <faelelf2@gmail.com>
2021-04-02 13:34:42 -0300

    Propriedades

--- CursoCSharp/ClassesEMetodos/Props.cs
+++ CursoCSharp/ClassesEMetodos/Props.cs
@@ -2,7 +2,7 @@ namespace CursoCSharp.ClassesEMetodos
 {
   public class CarroOpcional
   {
-    double desconto = 0.1;
+    double desconto = 0.5;
 
     string nome;
 
@@ -37,7 +37,7 @@ namespace CursoCSharp.ClassesEMetodos
       var op1 = new CarroOpcional("Ar Condicionado", 3499.9);
             op1.Nome = "Queixo Duro";
             op1.Preco = 1000;
-        System.Console.WriteLine(op1.Nome);
+        System.Console.WriteLine(op1);
         System.Console.WriteLine(op1.PrecoComDesconto);
     }
   }
