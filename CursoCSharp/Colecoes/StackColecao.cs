96e06aa69ba6ca6fe493b42550a2f85daa0f5385
Raphael Caputo <faelelf2@gmail.com>
2021-04-02 22:26:51 -0300

    Stack

--- CursoCSharp/Colecoes/StackColecao.cs
+++ CursoCSharp/Colecoes/StackColecao.cs
@@ -1,9 +1,38 @@
+using System;
+using System.Collections;
+using System.Text;
+
 namespace CursoCSharp.Colecoes
+
 {
-    public class StackColecao
+  public class StackColecao
+  {
+    public static void Executar()
     {
-          public static void Executar(){
-                    System.Console.WriteLine("Stack Criada!");
-        }
+      var pilha = new Stack();
+
+      pilha.Push(3);
+      pilha.Push("a");
+      pilha.Push(true);
+      pilha.Push(3.14f);
+
+
+      foreach (var item in pilha)
+      {
+        System.Console.WriteLine($"{item} ");
+      }
+
+      System.Console.WriteLine($"\nPop: {pilha.Pop()}");
+
+      foreach (var item in pilha)
+      {
+        System.Console.WriteLine($"{item} ");
+      }
+
+
+      System.Console.WriteLine($"\nPeek: {pilha.Peek()}");
+      System.Console.WriteLine(pilha.Count);
+
     }
+  }
 }
