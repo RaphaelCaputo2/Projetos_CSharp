f92893aa06d1281c5d5cb2438ffaaafab7ba6de5
Raphael Caputo <faelelf2@gmail.com>
2021-04-02 22:20:18 -0300

    GetHashCode

--- CursoCSharp/Colecoes/IgualdadeColecao.cs
+++ CursoCSharp/Colecoes/IgualdadeColecao.cs
@@ -1,9 +1,13 @@
 namespace CursoCSharp.Colecoes
 {
-    public class IgualdadeColecao
+  public class IgualdadeColecao
+  {
+    public static void Executar()
     {
-          public static void Executar(){
-                    System.Console.WriteLine("Igualdade Criada!");
-        }
+      var p1 = new Produto("A", 1);
+      var p2 = new Produto("A", 1);
+      System.Console.WriteLine(p1 == p2);
+
     }
+  }
 }
